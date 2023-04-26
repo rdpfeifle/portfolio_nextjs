227d52df199d81168b322c298348254d6287664d
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-04-25 22:23:58 -0600

    added basic navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,20 @@
-function Navbar() {
-  return <div>Navbar</div>;
+import Link from "next/link";
+
+const { default: Image } = require("next/image");
+
+export default function Navbar() {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="/../public/assets" alt="/" width="100" height="100" />
+      </div>
+      <div>
+        <ul>
+          <Link href="/">
+            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
 }
