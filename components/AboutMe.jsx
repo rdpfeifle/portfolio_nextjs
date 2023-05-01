4c79276a8eb26235f61ce2cf70e5752dd5f1c47e
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-05-01 14:09:25 -0600

    Added AboutMe section

---
+++ components/AboutMe.jsx
@@ -0,0 +1,37 @@
+export const AboutMe = () => {
+  return (
+    <div className="w-full bg-[#FFD15C]">
+      <div className="container">
+        <h2 className="py-8 px-0 underlineEffect">About Me</h2>
+        <p className="leading-relaxed pb-4">
+          <span className="font-semibold">
+            From Brazil to the US, my journey has been defined by a relentless
+            pursuit of dreams
+          </span>
+          . As a teenager, I set my sights on living abroad and making the
+          United States my new home. Overcoming language barriers and financial
+          struggles, I learned English on my own, and became a cultural exchange
+          student.
+        </p>
+        <p className="leading-relaxed pb-4">
+          In this land of opportunity, I discovered a world full of diverse
+          cultures and inspiring individuals. Although I initially studied
+          marketing in college, my passion for technology always burned within
+          me. Guided by my childhood tinkering with computers and building
+          projects, I enrolled in a coding bootcamp and became a software
+          engineer.
+        </p>
+        <p>
+          <span className="font-semibold">
+            My journey has taught me that with unwavering dedication and a
+            willingness to face challenges head-on, no dream is out of reach
+          </span>
+          . I hope to inspire others to fearlessly pursue their aspirations,
+          knowing that each obstacle encountered is an opportunity for growth.
+          As I embark on the next chapter of my adventure, I am filled with
+          excitement for the future and the unknown paths it holds.
+        </p>
+      </div>
+    </div>
+  );
+};
