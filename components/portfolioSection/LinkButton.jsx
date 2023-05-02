c3a6b845873f65c65e788aaab47ca3ed44efb72b
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-05-02 11:10:41 -0600

    Created LinkButton component for reusability and less repetition

---
+++ components/portfolioSection/LinkButton.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export const LinkButton = ({ linkSrc, iconSrc, iconAlt, label }) => {
+  return (
+    <Link href={linkSrc} target="_blank" rel="noopener noreferrer">
+      <button className="px-6 py-2">
+        <Image
+          src={iconSrc}
+          alt={iconAlt}
+          width={20}
+          height={20}
+          className="inline-block iconForBtn"
+        />{" "}
+        {label}
+      </button>
+    </Link>
+  );
+};
