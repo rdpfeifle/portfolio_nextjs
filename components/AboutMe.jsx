b01304f849e9226ae979bbdf9d1379f5dff8e751
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-05-02 20:00:36 -0600

    Changed content

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,35 +1,32 @@
 export const AboutMe = () => {
   return (
-    <section id="about" className="bg-yellow">
+    <section id="about" className="bg-customYellow">
       <div className="container">
         <h2 className="py-8 px-0 underlineEffect">About Me</h2>
         <p className="leading-relaxed pb-4">
           <span className="font-semibold">
-            From Brazil to the US, my journey has been defined by a relentless
-            pursuit of dreams
-          </span>
-          . As a teenager, I set my sights on living abroad and making the
-          United States my new home. Overcoming language barriers and financial
-          struggles, I learned English on my own, and became a cultural exchange
-          student.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </span>{" "}
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
+          repellat? Quam fugit eligendi veritatis facilis sed deserunt inventore
+          dolores voluptate adipisci quod similique ipsa quaerat omnis
+          assumenda, eaque architecto perspiciatis.
         </p>
-        <p className="leading-relaxed pb-4">
-          In this land of opportunity, I discovered a world full of diverse
-          cultures and inspiring individuals. Although I initially studied
-          Marketing in college, my passion for technology always burned within
-          me. Guided by my childhood tinkering with computers and building
-          projects, I enrolled in a coding bootcamp and became a Software
-          Engineer.
+        <p className="pb-4">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt
+          itaque quibusdam at cupiditate architecto perspiciatis labore, sit
+          ipsum! Eveniet tempore aliquid dolorum recusandae dolore vel velit
+          nostrum odit blanditiis quae.
         </p>
         <p>
           <span className="font-semibold">
-            My journey has taught me that with unwavering dedication and a
-            willingness to face challenges head-on, no dream is out of reach
-          </span>
-          . I hope to inspire others to fearlessly pursue their aspirations,
-          knowing that each obstacle encountered is an opportunity for growth.
-          As I embark on the next chapter of my adventure, I am filled with
-          excitement for the future and the unknown paths it holds.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+            iure cumque!
+          </span>{" "}
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt,
+          iure cumque! Nemo recusandae quaerat labore necessitatibus nesciunt
+          expedita tenetur consequatur alias cupiditate sapiente excepturi
+          officia temporibus, explicabo error earum iste!
         </p>
       </div>
     </section>
