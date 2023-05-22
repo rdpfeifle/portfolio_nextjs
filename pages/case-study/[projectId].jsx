04bc2a52a5eba949cc6446ae99e81d6f039b04cf
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-05-21 22:07:46 -0600

    Made case study's pages more SEO friendly

--- pages/case-study/[projectId].jsx
+++ pages/case-study/[projectId].jsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { projectsData } from "../../data/projectsData";
@@ -24,49 +25,55 @@ export default function ProjectPage({ project }) {
   const responsiveDiv = `sm:w-1/3 mt-2`;
 
   return (
-    <section className="container max-md:mt-12">
-      <h2>{projectName}</h2>
-      <p>{readTime} minute read</p>
-      <p>{shortDefinition}</p>
-      <div className={responsiveDiv}>
-        <h3>Tech Stack</h3>
-        <p>{stack}</p>
-      </div>
-      {live && (
+    <>
+      <Head>
+        <title>{projectName}&apos;s Case Study</title>
+        <meta name="description" content={shortDefinition} />
+      </Head>
+      <section className="container max-md:mt-12">
+        <h2>{projectName}</h2>
+        <p>{readTime} minute read</p>
+        <p>{shortDefinition}</p>
         <div className={responsiveDiv}>
-          <h3>Live</h3>
-          <Link href={live}>
-            <button className="px-5 py-3">
-              <Image
-                src="/assets/link-solid.svg"
-                alt="Link icon"
-                width={20}
-                height={20}
-                className="inline iconForBtn"
-              />{" "}
-              See live
-            </button>
-          </Link>
+          <h3>Tech Stack</h3>
+          <p>{stack}</p>
         </div>
-      )}
-      <Image
-        src={image}
-        alt={projectName}
-        width={450}
-        height={450}
-        className="rounded"
-      />
-      <h2 className="pt-10 pb-4">{objectives.title}</h2>
-      <p className="mb-10">{objectives.description}</p>
-      <h2 className="pb-4">{challengesAndSolutions.title}</h2>
-      <p className="mb-10">{challengesAndSolutions.description}</p>
-      <h2 className="pb-4">{technicalOverview.title}</h2>
-      <p className="mb-10">{technicalOverview.description}</p>
-      <h2 className="pb-4">{futureChanges.title}</h2>
-      <p className="mb-10">{futureChanges.description}</p>
-      <h2 className="pb-4">{results.title}</h2>
-      <p>{results.description}</p>{" "}
-    </section>
+        {live && (
+          <div className={responsiveDiv}>
+            <h3>Live</h3>
+            <Link href={live}>
+              <button className="px-5 py-3">
+                <Image
+                  src="/assets/link-solid.svg"
+                  alt="Link icon"
+                  width={20}
+                  height={20}
+                  className="inline iconForBtn"
+                />{" "}
+                See live
+              </button>
+            </Link>
+          </div>
+        )}
+        <Image
+          src={image}
+          alt={projectName}
+          width={450}
+          height={450}
+          className="rounded"
+        />
+        <h2 className="pt-10 pb-4">{objectives.title}</h2>
+        <p className="mb-10">{objectives.description}</p>
+        <h2 className="pb-4">{challengesAndSolutions.title}</h2>
+        <p className="mb-10">{challengesAndSolutions.description}</p>
+        <h2 className="pb-4">{technicalOverview.title}</h2>
+        <p className="mb-10">{technicalOverview.description}</p>
+        <h2 className="pb-4">{futureChanges.title}</h2>
+        <p className="mb-10">{futureChanges.description}</p>
+        <h2 className="pb-4">{results.title}</h2>
+        <p>{results.description}</p>{" "}
+      </section>
+    </>
   );
 }
 
