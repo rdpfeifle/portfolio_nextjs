7ef0fd4d622977b839d4fb553fef9fa1a58cb335
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2023-06-11 16:45:17 -0600

    Updated next.js version and added projects data

---
+++ data/projectsData.js
@@ -0,0 +1,111 @@
+export const projectsData = [
+  {
+    projectId: "tripaholics",
+    projectName: "Tripaholics",
+    shortDefinition:
+      "Tripaholics is a travel web application developed using Python, Flask, and JavaScript. It provides users with account management functionalities, allowing them to create new accounts or log into existing ones. The app offers a comprehensive set of features for trip management, enabling users to create, edit, and delete trips.",
+    stack: "Python, Flask, JavaScript",
+    live: "",
+    image: "/assets/images/coming-soon.png",
+    mockupImg1: "",
+    mockupImg2: "",
+    objectives: {
+      title: "Objectives",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    challengesAndSolutions: {
+      title: "Challenges and Solutions",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    technicalOverview: {
+      title: "Technical Overview",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    futureChanges: {
+      title: "Future Changes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    results: {
+      title: "Results",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  },
+  {
+    projectId: "alloy-ecommerce",
+    projectName: "e-Commerce Store - Alloy Fashion",
+    shortDefinition:
+      "Welcome to Alloy Fashion, an eCommerce website powered by Shopify. Experience a comprehensive online shopping solution that combines a user-friendly product page, convenient cart functionality, and a newsletter feature. With effortless navigation and secure payment processing, Alloy Fashion provides a smooth and secure shopping experience.",
+    stack: "Shopify",
+    live: "",
+    image: "/assets/images/alloyStore-mockup.png",
+    mockupImg1: "/assets/images/alloy-mockup01.png",
+    mockupImg2: "/assets/images/alloy-mockup02.png",
+    mockupImg3: "/assets/images/alloy-pages.jpg",
+    objectives: {
+      title: "Objectives",
+      description:
+        "Leveraging the powerful features of the Shopify platform, the primary objectives were to create smooth and secure shopping experiences that engage and convert customers, through the integration of innovative features, design elements, and meticulous attention to detail.",
+    },
+    challengesAndSolutions: {
+      title: "Challenges and Solutions",
+      description: [
+        "During the development of this website, a challenge emerged in creating an aesthetically pleasing and intuitive user interface. To address this, a premium theme was carefully chosen and integrated with custom design elements, resulting in a visually stunning experience. The utilization of Shopify's online store 2.0 features, including sections everywhere, allowed for a flexible and easily manageable content structure, showcasing an ability to deliver a user-friendly experience.",
+        "Another challenge involved integrating essential functionalities commonly found in successful eCommerce websites. To tackle this, features such as a fully functional shopping cart, support for multiple payment methods (including credit cards and PayPal), SSL-encrypted checkout for secure transactions, and automated email notifications for customers and fulfillment services were smoothly incorporated. These solutions exemplify the capacity to create comprehensive and cohesive shopping experiences for users.",
+      ],
+    },
+    technicalOverview: {
+      title: "Technical Overview",
+      description: `Alloy was built using Shopify as the foundation, customized with Shopify Liquid and CSS. These powerful technologies were utilized to create visually appealing and interactive user interfaces. Integration of advanced features, such as the captivating "Shop the Look" feature on the homepage, product reviews, automatic collections, and advanced filter and search capabilities, demonstrates expertise in leveraging Shopify's capabilities for enhanced user engagement and functionality.`,
+    },
+    futureChanges: {
+      title: "Future Changes",
+      description:
+        "Moving forward, the plan is to continually explore and experiment with design elements and features on this website. This may involve exploring different themes and layouts, incorporating emerging technologies such as augmented reality product visualization, or implementing advanced personalization features. By consistently pushing boundaries and staying updated with the latest trends, the aim is to demonstrate adaptability and evolution as a skilled designer and developer.",
+    },
+    results: {
+      title: "Results",
+      description:
+        "Alloy is an impressive eCommerce website built with Shopify, showcasing expertise in design and development. Leveraging Shopify's advanced features, Alloy excels in delivering captivating eCommerce experiences. By incorporating powerful tracking and analytics tools like Google Analytics and Facebook Pixel, Alloy provides valuable insights into user behavior, demographics, and conversion rates. This data-driven approach allows for targeted marketing campaigns, personalized user experiences, and optimized conversion rates, ultimately driving business growth. Additionally, Alloy's compatibility with Shopify's online store 2.0 features enables compelling visual experiences and flexible design options, further enhancing the overall user experience.",
+    },
+  },
+  {
+    projectId: "portfolio-website",
+    projectName: "Portfolio Website",
+    shortDefinition: "A project to do something",
+    stack: "Next.js, React.js, JavaScript, Tailwind CSS",
+    live: "",
+    image: "/assets/images/portfolio-mockup.png",
+    mockupImg1: "",
+    mockupImg2: "",
+    objectives: {
+      title: "Objectives",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    challengesAndSolutions: {
+      title: "Challenges and Solutions",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    technicalOverview: {
+      title: "Technical Overview",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    futureChanges: {
+      title: "Future Changes",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    results: {
+      title: "Results",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  },
+];
